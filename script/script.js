322a1da44571ae5b31470b9d52f05f0837596aa2
hamid331994 <hamid331994@gmial.com>
2019-07-01 09:56:20 +0430

    a

--- script/script.js
+++ script/script.js
@@ -1,3 +1,30 @@
+// // VARS
+const dots = document.querySelectorAll('.page-number-section');
+
+// // FUNCTIONAL VARS:
+let activeId = 1;
+
 document.addEventListener('scroll', (event) => {
-  console.log(event.type)
+  // console.log(event.type)
+});
+
+function whereIsActive() {
+  // console.log(dots);
+}
+
+// // FUNCTIONS TO RUNB ON PAGE LOAD:
+whereIsActive(1);
+
+$('.pagenav a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+    console.log('ha,mid')
+
+    const hash = this.hash;
+
+    $('html, body')
+      .animate({
+        scrollTop: $(hash).offset().top
+      },3000);
+  }
 });
